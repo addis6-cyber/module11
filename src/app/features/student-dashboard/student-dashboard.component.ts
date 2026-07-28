c5f52f16a4ac9d1b79234c19dddd7768337e7345
Addisu Sheko <adisusheko@gmail.com>
2026-07-28 04:53:08 -0400

    update m8-lab-session2

--- src/app/features/student-dashboard/student-dashboard.component.ts
+++ src/app/features/student-dashboard/student-dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component, signal, computed } from "@angular/core";
 
+import { CourseCardComponent } from '../../ui/course-card/course-card';
+import { Course } from '../../models/course.model';
+
 @Component({
-  selector: "app-student-dashboard",
+  selector: 'app-student-dashboard',
   standalone: true,
-  templateUrl: "./student-dashboard.component.html",
-  styleUrl: "./student-dashboard.component.scss",
+  imports: [CourseCardComponent],
+  templateUrl: './student-dashboard.component.html',
+  styleUrl: './student-dashboard.component.scss'
 })
 export class StudentDashboardComponent {
 
@@ -21,4 +25,49 @@ export class StudentDashboardComponent {
   registerForClass() {
     this.earnedCredits.update(c => c + 3);
   }
+  selectedCourse = signal<Course | null>(null);
+
+sampleCourse: Course = {
+  id: 1,
+  title: 'Advanced Java Services',
+  code: 'CSE-101',
+  maxCapacity: 30,
+  enrollmentCount: 12,
+};
+
+handleEnroll(course: Course) {
+  this.selectedCourse.set(course);
+  console.log('Enrollment requested for:', course.title);
+}
+availableCourses = signal<Course[]>([
+{
+id: 1,
+title: "Advanced Java Services",
+code: "CSE-101",
+maxCapacity: 30,
+enrollmentCount: 10,
+},
+{
+id: 2,
+title: "Angular UI Lab",
+code: "CSE-210",
+maxCapacity: 25,
+enrollmentCount: 25,
+},
+{
+id: 3,
+title: "Database Design",
+code: "CSE-305",
+maxCapacity: 20,
+enrollmentCount: 18,
+},
+{
+id: 4,
+title: "API Security Workshop",
+code: "CSE-420",
+maxCapacity: 40,
+enrollmentCount: 15,
+},
+]);
+
 }
---
+++ src/app/ui/course-card/course-card.ts
@@ -0,0 +1,14 @@
+import { Component, input, output } from '@angular/core';
+import { Course } from '../../models/course.model';
+
+@Component({
+  selector: 'tms-course-card',
+  standalone: true,
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.scss',
+})
+export class CourseCardComponent {
+  course = input.required<Course>();
+  enrollClicked = output<Course>();
+}
+
