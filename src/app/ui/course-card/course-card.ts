3bfa87cfa943dd192f89ae3434418e379c66fb10
Addisu Sheko <adisusheko@gmail.com>
2026-07-29 11:02:21 -0400

    update m8-lab-session2

--- src/app/ui/course-card/course-card.ts
+++ src/app/ui/course-card/course-card.ts
@@ -1,9 +1,11 @@
 import { Component, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Course } from '../../models/course.model';
 
 @Component({
   selector: 'tms-course-card',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './course-card.html',
   styleUrl: './course-card.scss',
 })
